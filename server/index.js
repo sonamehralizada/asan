38fc860fb7fb28abf5602e4b9c6655f069d69b39
Sona Mheralizada <141008448+sonamehralizada@users.noreply.github.com>
2024-09-03 10:21:04 +0400

    removed db connections

--- server/index.js
+++ server/index.js
@@ -1,6 +1,5 @@
 import express from 'express';
 import cors from 'cors';
-import postgresClient from './config/db.js';
 import userRouter from './routers/userRouter.js';
 
 const app = express();
@@ -10,18 +9,8 @@ app.use(cors());
 // Define routes after cors middleware
 app.use('/users', userRouter);
 
-
-
 const PORT = 5000;
 
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`);
-  postgresClient.connect(err => {
-    if (err) {
-      console.log('connection error', err.stack);
-    } else {
-      console.log('db connection successful');
-    }
-  });
 });
-
